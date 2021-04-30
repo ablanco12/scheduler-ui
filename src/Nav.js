4cd11eed2f0c196f2f66394bad84a28cbc777d73
Andrew-Joseph Blanco <andrewblanco2@outlook.com>
2021-04-29 21:57:37 -0500

    Updates made to nav

--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import MenuList from './MenuList'
+// import MenuList from './MenuList'
 import './Nav.css'
 
 function Nav() {
     return (
         <div className="nav__top">
             {/* <h1 className='nav__title'>AppGetaway</h1> */}
-            <img className="nav__title" src="https://github.com/ablanco12/scheduler-ui/blob/master/public/assets/appddgetaways_logo-02.png?raw=true" alt="" />
-            <MenuList />
+            <img className="nav__title" src="https://github.com/ablanco12/scheduler-ui/blob/master/public/assets/appddgetaways_logo-03.png?raw=true" alt="" />
+            {/* <MenuList /> */}
         </div>
     )
 }
